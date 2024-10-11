1da9f2300ee1c73fe7632588bb1513c9a4d4096f
Rosca Andrei <andrei_rosca03@yahoo.com>
2024-10-11 16:19:31 +0300

    json update

---
+++ script.js
@@ -0,0 +1,21 @@
+let projects;
+let cards2=document.querySelector(".cards2");
+fetch("https://andreirosca12.github.io/portofolio/myprojects.json")
+    .then(r=>r.json())
+    .then(json=>{
+        console.log(json);
+        projects=json;
+        projects.forEach(project=> {
+            cards2.innerHTML+=`
+                        <div class="card2">
+                            <img src="${project.url}" alt="">
+                            <h3>${project.titlu}</h3>
+                            <p>${project.descriere}</p>
+                            <div class="buttons">
+                                <a class="b1" href="https://github.com/andreirosca12/andreirosca12.github.io">Github</a>
+                                <a class="b2" href="https://andreirosca12.github.io/">Demo</a>
+                            </div>
+                        </div>   
+            `;
+        });
+    })
